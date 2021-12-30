cac1a4df6447ac7d00ca800b12b21cd22438d001
Meri Engel <mengel7@mchenry.edu>
2021-12-30 15:14:17 -0600

    Completed Learn 2

---
+++ learn02-completed/scripts/script.js
@@ -0,0 +1,9 @@
+function myFunction2() {
+    document.getElementById("sample").innerHTML =
+        "Changed from a function in an external script";
+}
+// single line comment
+
+/* 
+block comment
+*/
