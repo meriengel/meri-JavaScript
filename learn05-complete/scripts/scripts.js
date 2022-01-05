a0cf96894fa80bcba596e376bd0c5fa3c568f9d0
Meri Engel <mengel7@mchenry.edu>
2022-01-04 20:06:20 -0600

    finished

--- learn05-complete/scripts/scripts.js
+++ learn05-complete/scripts/scripts.js
@@ -1,6 +1,4 @@
 // Functions, objects, and events
-// Functions, objects, and events
-
 
 // Dog objects
 const dog1 = {
@@ -8,13 +6,35 @@ const dog1 = {
     image: "images/black-dog.jpg",
     caption: "Just hanging out waiting for water",
     altTag: "Black dog in bucket."
-
+};
+const dog2 = {
+    heading: "Golden Puppy",
+    image: "images/gold-dog.jpg",
+    caption: "Enjoying the Dog Days of Summer",
+    altTag: "Golden puppy on grass"
+};
+const dog3 = {
+    heading: "Lend me a hand",
+    image: "images/snow-dog.jpg",
+    caption: "I think you took MY stick!",
+    altTag: "cartoon dog taking stick arm from snow man."
+};
+const dog4 = {
+    heading: "Sweet hearts",
+    image: "images/white-dogs.jpg",
+    caption: "Summer lovin...having a blast!",
+    altTag: "Two white dogs at the  beach."
 };
 
 function chooseDog(dog) {
     if (dog == 1) {
-        loadMe(dog1, 1) // don't put dog1 in quotes or it will pass a string not the object
-
+        loadMe(dog1, 1); // don't put dog1 in quotes or it will pass a string not the object
+    } else if (dog == 2) {
+        loadMe(dog2, 2);
+    } else if (dog == 3) {
+        loadMe(dog3, 3);
+    } else if (dog == 4) {
+        loadMe(dog4, 4)
     }
 
 }
