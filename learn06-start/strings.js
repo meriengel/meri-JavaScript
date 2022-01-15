5dd4f5f835f8f93de8605b0afb2b2f81d82cb891
Meri Engel <mengel7@mchenry.edu>
2022-01-15 17:22:33 -0600

    objects project sample

---
+++ learn06-start/strings.js
@@ -0,0 +1,52 @@
+// follow the directions in the comments
+// use console.log() when printing to the console
+
+// from JavaScript Strings: https://www.w3schools.com/js/js_strings.asp
+
+
+// String length: calculate the string length and display to console
+let myString = "Be it ever so humble, there is no place like home."
+
+
+// Escape sequences
+// print a string to the console that uses three different escape sequences (look at the web page)
+
+
+// JavaScript String Methods https://www.w3schools.com/js/js_string_methods.asp
+
+// create a variable named 'name' and store your complete name in it
+// Use the JavaScript slice function to display your first name to the console
+
+
+// Use the Javascript substring Method to display your middle name to the console
+
+
+// Use the JavaScript subst Method to display your last name to the console
+
+
+
+// replacing string content - in the following variable use the replace method to put your major 
+// in a new string and print to the console
+
+let major = "I'm majoring in Physical Therapy at McHenry County College"
+
+
+// convert the following variable to upper case then print to the console
+let myText = "It\'s a great day to be alive!"
+
+// Use the trim() method to remove white spaces from the following variable and print to the
+// console
+let trimText = "                  McHenry County College                "
+
+// use the indexOf() method to get the index of 'May' and print to the console
+
+let months = "January, February, March, April, May, June, July, August, September, October, November, December"
+
+
+// JavaScript Template Literals   https://www.w3schools.com/js/js_string_templates.asp
+
+// create a template literals for both your first and last name (name the variables first and last)
+
+
+// Use Variable substitutions to create a greeting that incorporates your first and last name and 
+// display it to the console
