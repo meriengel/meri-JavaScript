f6278e281aeb8f052087c31ecd5c20c90797beae
Meri Engel <mengel7@mchenry.edu>
2022-01-22 15:05:13 -0600

    learn 7 upload

---
+++ learn07-start/learn07.js
@@ -0,0 +1,62 @@
+/*
+    read/watch the associated reference and complete the directions in the comments.
+
+*/
+
+/* JS Numbers: https://www.w3schools.com/js/js_numbers.asp
+    We will focus on the things with numbers that are significantly
+    different than python. Please read the page linked to above before 
+    completing the following:
+*/
+
+/* Write a line of code demonstrating what will happen if 
+   you try to add a number and a string. Use console.log() to 
+   display both the equation and the results. 
+*/
+
+
+/*
+    JS Number Methods: https://www.w3schools.com/js/js_number_methods.asp
+*/
+
+// Demonstrate the use of toString()
+
+
+// Demonstrate the use of toExponential()
+
+
+// Demonstrate the use of toFixed()
+
+
+// Demonstrate the use of toPrecision()
+
+
+// Demonstrate the use of parseFloat()
+
+
+// Demonstrate the use of parseInt()
+
+
+
+// Watch Simple Comparisons: https://www.linkedin.com/learning/learning-the-javascript-language-2/simple-comparisons?autoplay=true&resume=false&u=69317474
+
+/* In a comment explain your understanding of the difference between using == and ===  (You will probably
+need to do an additional web search to really get it.)
+*/
+
+// Watch Arithmetic Operators: https://www.linkedin.com/learning/learning-the-javascript-language-2/arithmetic-operators?autoplay=true&resume=false&u=69317474
+// Watch Logical Operators: https://www.linkedin.com/learning/learning-the-javascript-language-2/logical-operators?autoplay=true&resume=false&u=69317474
+// In a comment describe the order of precedence for and / or operators
+
+
+
+// Watch Conditional If: https://www.linkedin.com/learning/learning-the-javascript-language-2/logical-operators?autoplay=true&resume=false&u=69317474
+// Watch Conditionals Switch: https://www.linkedin.com/learning/learning-the-javascript-language-2/conditionals-switch?autoplay=true&resume=false&u=69317474
+
+
+// Write code to demonstrate the use of the switch statement
+
+
+// Watch Terse ifs: https://www.linkedin.com/learning/learning-the-javascript-language-2/terse-ifs?autoAdvance=true&autoSkip=true&autoplay=true&resume=false&u=69317474
+// Watch Ternary Operator:https://www.linkedin.com/learning/learning-the-javascript-language-2/ternary-operator?autoplay=true&resume=false&u=69317474
+// Write code to demonstrate the use of the ternary operator
