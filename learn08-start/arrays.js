57b968bac85a46325f3734d8b59ba8fecb476c90
Meri Engel <mengel7@mchenry.edu>
2022-02-05 17:46:37 -0600

    learn 7 initial commit

---
+++ learn08-start/arrays.js
@@ -0,0 +1,35 @@
+/* Follow the directions in the comments to demonstrate the use of arrays */
+
+/* JavaScript Arrays:https://www.w3schools.com/js/js_arrays.asp */
+
+// Create an array called months that holds all the months of the year
+
+
+// use the index to print the month May from the months array to the console
+
+
+// print the length of the month array to the console
+
+
+// Loop through all of the months array and print each month to the console
+
+//JS Array Methods:https://www.w3schools.com/js/js_array_methods.asp
+
+
+// convert the months array to a string and print to the console
+
+
+// create two new arrays - weekends and weekdays, add the appropriate days to each
+
+
+// join the two arrays into a new array - week
+
+
+// print the contents of week to the console
+
+
+// use the pop method to remove the last element of the week array, then print
+// the week array to the console
+
+let week = ["Sunday", "Monday", "Tuesday"]
+console.log(week)
