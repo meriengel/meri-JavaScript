5ab9ce6324f5ffce1d5112c5120e67cda850daef
Meri Engel <mengel7@mchenry.edu>
2022-02-05 19:08:48 -0600

    update index pages

--- learn06-start/strings.js
+++ learn06-start/strings.js
@@ -1,52 +1,57 @@
-// follow the directions in the comments
-// use console.log() when printing to the console
+function start() {
+    // follow the directions in the comments
+    // print to the index page
 
-// from JavaScript Strings: https://www.w3schools.com/js/js_strings.asp
+    // print your name to the name heading
+    document.getElementById("name").innerHTML = "Your Name here";
 
-
-// String length: calculate the string length and display to console
-let myString = "Be it ever so humble, there is no place like home."
-
-
-// Escape sequences
-// print a string to the console that uses three different escape sequences (look at the web page)
+    // from JavaScript Strings: https://www.w3schools.com/js/js_strings.asp
 
 
-// JavaScript String Methods https://www.w3schools.com/js/js_string_methods.asp
-
-// create a variable named 'name' and store your complete name in it
-// Use the JavaScript slice function to display your first name to the console
+    // String length: calculate the string length and print to the length paragraph
+    let myString = "Be it ever so humble, there is no place like home."
 
 
-// Use the Javascript substring Method to display your middle name to the console
+    // Escape sequences
+    // print a string to the escape paragraph that uses three different escape sequences (look at the web page)
 
 
-// Use the JavaScript subst Method to display your last name to the console
+    // JavaScript String Methods https://www.w3schools.com/js/js_string_methods.asp
+
+    // create a variable named 'name' and store your complete name in it
+    // Use the JavaScript slice function to display your first name to first paragraph
+
+
+    // Use the Javascript substring Method to display your middle name to the middle paragraph
+
+
+    // Use the JavaScript subst Method to display your last name to the last paragraph
 
 
 
-// replacing string content - in the following variable use the replace method to put your major 
-// in a new string and print to the console
+    // replacing string content - in the following variable use the replace method to put your major 
+    // in a new string and print to the major paragraph
 
-let major = "I'm majoring in Physical Therapy at McHenry County College"
-
-
-// convert the following variable to upper case then print to the console
-let myText = "It\'s a great day to be alive!"
-
-// Use the trim() method to remove white spaces from the following variable and print to the
-// console
-let trimText = "                  McHenry County College                "
-
-// use the indexOf() method to get the index of 'May' and print to the console
-
-let months = "January, February, March, April, May, June, July, August, September, October, November, December"
+    let major = "I'm majoring in Physical Therapy at McHenry County College"
 
 
-// JavaScript Template Literals   https://www.w3schools.com/js/js_string_templates.asp
+    // convert the following variable to upper case then print to the upper paragraph
+    let myText = "It\'s a great day to be alive!"
 
-// create a template literals for both your first and last name (name the variables first and last)
+    // Use the trim() method to remove white spaces from the following variable and print to the
+    // trim paragraph
+    let trimText = "                  McHenry County College                "
+
+    // use the indexOf() method to get the index of 'May' and print the index number to the index paragraph
+
+    let months = "January, February, March, April, May, June, July, August, September, October, November, December"
 
 
-// Use Variable substitutions to create a greeting that incorporates your first and last name and 
-// display it to the console
+    // JavaScript Template Literals   https://www.w3schools.com/js/js_string_templates.asp
+
+    // create a template literals for both your first and last name (name the variables first and last)
+
+
+    // Use Variable substitutions to create a greeting that incorporates your first and last name and 
+    // print to the greeting paragraph
+}
