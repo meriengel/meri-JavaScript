8b463ee64b2035ca08e2272357ee3996be8b3489
Meri Engel <mengel7@mchenry.edu>
2022-02-05 18:42:45 -0600

    fixed files

--- learn07-start/learn07.js
+++ learn07-start/learn07.js
@@ -1,62 +1,66 @@
-/*
-    read/watch the associated reference and complete the directions in the comments.
+function start() {
 
-*/
+    /*
+        read/watch the associated reference and complete the directions in the comments.
 
-/* JS Numbers: https://www.w3schools.com/js/js_numbers.asp
-    We will focus on the things with numbers that are significantly
-    different than python. Please read the page linked to above before 
-    completing the following:
-*/
+    */
 
-/* Write a line of code demonstrating what will happen if 
-   you try to add a number and a string. Use console.log() to 
-   display both the equation and the results. 
-*/
+    /* JS Numbers: https://www.w3schools.com/js/js_numbers.asp
+        We will focus on the things with numbers that are significantly
+        different than python. Please read the page linked to above before 
+        completing the following:
+    */
 
-
-/*
-    JS Number Methods: https://www.w3schools.com/js/js_number_methods.asp
-*/
-
-// Demonstrate the use of toString()
+    /* Write a line of code demonstrating what will happen if 
+       you try to add a number and a string. Use console.log() to 
+       display both the equation and the results. 
+    */
 
 
-// Demonstrate the use of toExponential()
+    /*
+        JS Number Methods: https://www.w3schools.com/js/js_number_methods.asp
+    */
+
+    // Demonstrate the use of toString()
 
 
-// Demonstrate the use of toFixed()
+    // Demonstrate the use of toExponential()
 
 
-// Demonstrate the use of toPrecision()
+    // Demonstrate the use of toFixed()
 
 
-// Demonstrate the use of parseFloat()
+    // Demonstrate the use of toPrecision()
 
 
-// Demonstrate the use of parseInt()
+    // Demonstrate the use of parseFloat()
 
 
-
-// Watch Simple Comparisons: https://www.linkedin.com/learning/learning-the-javascript-language-2/simple-comparisons?autoplay=true&resume=false&u=69317474
-
-/* In a comment explain your understanding of the difference between using == and ===  (You will probably
-need to do an additional web search to really get it.)
-*/
-
-// Watch Arithmetic Operators: https://www.linkedin.com/learning/learning-the-javascript-language-2/arithmetic-operators?autoplay=true&resume=false&u=69317474
-// Watch Logical Operators: https://www.linkedin.com/learning/learning-the-javascript-language-2/logical-operators?autoplay=true&resume=false&u=69317474
-// In a comment describe the order of precedence for and / or operators
+    // Demonstrate the use of parseInt()
 
 
 
-// Watch Conditional If: https://www.linkedin.com/learning/learning-the-javascript-language-2/logical-operators?autoplay=true&resume=false&u=69317474
-// Watch Conditionals Switch: https://www.linkedin.com/learning/learning-the-javascript-language-2/conditionals-switch?autoplay=true&resume=false&u=69317474
+    // Watch Simple Comparisons: https://www.linkedin.com/learning/learning-the-javascript-language-2/simple-comparisons?autoplay=true&resume=false&u=69317474
+
+    /* In a comment explain your understanding of the difference between using == and ===  (You will probably
+    need to do an additional web search to really get it.)
+    */
+
+    // Watch Arithmetic Operators: https://www.linkedin.com/learning/learning-the-javascript-language-2/arithmetic-operators?autoplay=true&resume=false&u=69317474
+    // Watch Logical Operators: https://www.linkedin.com/learning/learning-the-javascript-language-2/logical-operators?autoplay=true&resume=false&u=69317474
+    // In a comment describe the order of precedence for and / or operators
 
 
-// Write code to demonstrate the use of the switch statement
+
+    // Watch Conditional If: https://www.linkedin.com/learning/learning-the-javascript-language-2/logical-operators?autoplay=true&resume=false&u=69317474
+    // Watch Conditionals Switch: https://www.linkedin.com/learning/learning-the-javascript-language-2/conditionals-switch?autoplay=true&resume=false&u=69317474
 
 
-// Watch Terse ifs: https://www.linkedin.com/learning/learning-the-javascript-language-2/terse-ifs?autoAdvance=true&autoSkip=true&autoplay=true&resume=false&u=69317474
-// Watch Ternary Operator:https://www.linkedin.com/learning/learning-the-javascript-language-2/ternary-operator?autoplay=true&resume=false&u=69317474
-// Write code to demonstrate the use of the ternary operator
+    // Write code to demonstrate the use of the switch statement
+
+
+    // Watch Terse ifs: https://www.linkedin.com/learning/learning-the-javascript-language-2/terse-ifs?autoAdvance=true&autoSkip=true&autoplay=true&resume=false&u=69317474
+    // Watch Ternary Operator:https://www.linkedin.com/learning/learning-the-javascript-language-2/ternary-operator?autoplay=true&resume=false&u=69317474
+    // Write code to demonstrate the use of the ternary operator
+
+}
--- learn08-start/arrays.js
+++ learn08-start/arrays.js
@@ -1,35 +1,55 @@
-/* Follow the directions in the comments to demonstrate the use of arrays */
+function start() {
 
-/* JavaScript Arrays:https://www.w3schools.com/js/js_arrays.asp */
+    /* Follow the directions in the comments to demonstrate the use of arrays */
 
-// Create an array called months that holds all the months of the year
+    /* JavaScript Arrays:https://www.w3schools.com/js/js_arrays.asp */
+    // write your name to the name h1
 
-
-// use the index to print the month May from the months array to the console
-
-
-// print the length of the month array to the console
+    // Create an array called months that holds all the months of the year
 
 
-// Loop through all of the months array and print each month to the console
-
-//JS Array Methods:https://www.w3schools.com/js/js_array_methods.asp
+    // use the index to print the month May from the months array the may paragraph
 
 
-// convert the months array to a string and print to the console
+    // print the length of the month array to the length paragraph
 
 
-// create two new arrays - weekends and weekdays, add the appropriate days to each
+    // Loop through all of the months array and print each month to the months paragraph
+
+    //JS Array Methods:https://www.w3schools.com/js/js_array_methods.asp
 
 
-// join the two arrays into a new array - week
+    // convert the months array to a string and print to the string paragraph
 
 
-// print the contents of week to the console
+    // create two new arrays - weekends and weekdays, add the appropriate days to each
 
 
-// use the pop method to remove the last element of the week array, then print
-// the week array to the console
+    // join the two arrays into a new array - week
 
-let week = ["Sunday", "Monday", "Tuesday"]
-console.log(week)
+
+    // print the contents of week to the week paragraph
+
+
+    // use the pop method to remove the last element of the week array, then print
+    // the week array to the pop paragraph
+
+
+    // use the push function to add two fruits to the array fruits print to the fruits paragraph
+    fruits = ["Apple", "Kiwi", "Mango"]
+
+
+    // use the shift function to add a fruit to the front of the list and print to the fruits2 paragraph
+
+    // JS Array Sort  https://www.w3schools.com/js/js_array_sort.asp
+
+    languages = ["COBOL", "livescript", "C++", "Python", "JavaScript", "Lua", "Basic"]
+        // sort the array languages and print to the paragraph sorted
+        // reverse the array languages and print to the paragraph reversed
+
+    // use the compare function to sort the numbers  and print to the num-sorted paragraph
+    numbers = [42, 23, 21, 50, 20, 6, 31, 9]
+
+    // Read the Array Iteration page https://www.w3schools.com/js/js_array_iteration.asp 
+    // read the Array Const page https://www.w3schools.com/js/js_array_const.asp
+}
