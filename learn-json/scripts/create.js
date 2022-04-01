42480971465c8aa4027361a4762db789b42c6db2
Meri Engel <mengel7@mchenry.edu>
2022-03-31 20:14:16 -0500

    start

---
+++ learn-json/scripts/create.js
@@ -0,0 +1,10 @@
+let contacts = [];
+
+function addme(){
+    // get your form items, create an object
+    // and add to contacts
+}
+
+function create(){
+   // save the output file as output.json!!! 
+}
