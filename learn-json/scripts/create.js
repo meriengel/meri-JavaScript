9ed1e316f95642018c4cbbd2a378b8e11684de2d
Meri Engel <mengel7@mchenry.edu>
2022-03-31 21:28:20 -0500

    fixed json

--- learn-json/scripts/create.js
+++ learn-json/scripts/create.js
@@ -1,10 +1,26 @@
 let contacts = [];
 
-function addme(){
+function addme() {
     // get your form items, create an object
     // and add to contacts
-}
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
 
-function create(){
-   // save the output file as output.json!!! 
+
+    let contact = {};
+    contact.fname = fname;
+    contact.lname = lname;
+    contact.email = email;
+    contact.phone = phone;
+
+    let myJSON = JSON.stringify(contact);
+    document.getElementById("json-value").innerHTML = myJSON;
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+
+
 }
